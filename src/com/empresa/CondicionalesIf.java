60c6f67fae38ab703524a2db7b25b0a767ead38a
Restrepo <andres.restrepo@softwareone.com>
2025-04-28 22:33:41 -0500

    second commit 4/28/2025 - Creacion de clase Condicionales

---
+++ src/com/empresa/CondicionalesIf.java
@@ -0,0 +1,40 @@
+package com.empresa;
+
+public class CondicionalesIf {
+
+	public static void main(String[] args) {
+		String var1 = "Hola";
+		String var2 = "Equipo";
+		
+		int a = 100;
+		int b = 200;
+		
+		
+		/*if(a>1000)
+		{
+			System.out.println("A es mayor que 1000");
+		}*/
+		/*if (a<1000) {
+			System.out.println("A es menor que 1000");
+		}
+		else
+		{
+			System.out.println("A es mayor que 1000");
+		}*/
+		
+		if (a==100) {
+			System.out.println("A es igual a 100");
+		}
+		else if (a==200) {
+			System.out.println("A es igual a 200");
+		}
+		else if (a==300) {
+			System.out.println("A es igual a 300");
+		}
+		else  {
+			System.out.println("A no es igual a ningun valor");
+		}
+
+	}
+
+}
