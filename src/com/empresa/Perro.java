e0f48560ba5d3c95bdcd5f18f1e43b6e3e88246b
Restrepo <andres.restrepo@softwareone.com>
2025-05-05 21:30:14 -0500

    Sexto commit 5/5/2025 Se inclute la clase perro

---
+++ src/com/empresa/Perro.java
@@ -0,0 +1,23 @@
+package com.empresa;
+
+import java.security.PublicKey;
+
+public class Perro {
+	String nombre="Maia";
+	String raza="Husky";
+	int altura=4;
+	
+	public void ladrar()
+	{
+		System.out.println(nombre + " " + "Esta ladrando");
+	}
+
+	public static void main(String[] args) {
+	
+		Perro objperro = new Perro();
+		System.out.println(objperro.altura);
+		objperro.ladrar();
+		
+	}
+
+}
